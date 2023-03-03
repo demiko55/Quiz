9c002699b5f9e94c9cbd761cfafcf7f724f13784
Demi <cbgaojunmeng@gmail.com>
2023-03-02 23:05:07 -0800

    add confetti effect

--- client/src/components/Quiz.jsx
+++ client/src/components/Quiz.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import confetti from 'canvas-confetti';
 
 const Quiz = ({ source, id }) => {
   // console.log('id', id);
@@ -10,6 +11,59 @@ const Quiz = ({ source, id }) => {
   let path = `../../../${source[page].src}`;
   let options = source[page].options[0];
   let optionsArr = options.split(',');
+
+  const celebrate = (e) => {
+    //console.log(e.target);
+    const trigger = e.target;
+    const defaults = {
+      disableForReducedMotion: true
+    };
+
+    function fire(particleRatio, opts) {
+      confetti(
+        Object.assign({}, defaults, opts, {
+          particleCount: Math.floor(200 * particleRatio)
+        })
+      );
+    }
+    function confettiExplosion(origin) {
+      fire(0.25, {
+        spread: 26,
+        startVelocity: 55,
+        origin
+      });
+      fire(0.2, {
+        spread: 60,
+        origin
+      });
+      fire(0.35, {
+        spread: 100,
+        decay: 0.91,
+        origin
+      });
+      fire(0.1, {
+        spread: 120,
+        startVelocity: 25,
+        decay: 0.92,
+        origin
+      });
+      fire(0.1, {
+        spread: 120,
+        startVelocity: 45,
+        origin
+      });
+    }
+    const rect = trigger.getBoundingClientRect();
+    const center = {
+      x: rect.left + rect.width / 2,
+      y: rect.top + rect.height / 2
+    };
+    const origin = {
+      x: center.x / window.innerWidth,
+      y: center.y / window.innerHeight
+    };
+    confettiExplosion(origin);
+  }
 
   let url = 'http://localhost:3010/score';
   const updateScore = () => {
@@ -35,7 +89,6 @@ const Quiz = ({ source, id }) => {
   const handleTime = (e) => {
     e.persist();
     // console.log('handleTime???', e.target.currentTime);
-
     timeSet = setTimeout(function () {
       // console.log('now???', e.target.currentTime);
       if (page < 10) {
@@ -45,15 +98,17 @@ const Quiz = ({ source, id }) => {
   }
 
   const handleAnswer = (e) => {
+    e.persist();
     clearTimeout(timeSet);
     if (source[page].answer == e.target.value) {
       //update the score in db
       updateScore();
       e.target.style.background = 'green';
-    }else{
+      celebrate(e);
+    } else {
       e.target.style.background = 'red';
     }
-    setTimeout(function(){
+    setTimeout(function () {
       if (page < 10) {
         setPage(page + 1);
       }
@@ -78,10 +133,10 @@ const Quiz = ({ source, id }) => {
             </audio>) : (<div className='title'><h1>{source[page].src}</h1></div>)
           }
           <div className='answer'>
-            <input type='button' onClick={handleAnswer} value={optionsArr[0]} />
-            <input type='button' onClick={handleAnswer} value={optionsArr[1]} />
-            <input type='button' onClick={handleAnswer} value={optionsArr[2]} />
-            <input type='button' onClick={handleAnswer} value={optionsArr[3]} />
+            <input type='button' onClick={handleAnswer} value={optionsArr[0]} className='options' />
+            <input type='button' onClick={handleAnswer} value={optionsArr[1]} className='options' />
+            <input type='button' onClick={handleAnswer} value={optionsArr[2]} className='options' />
+            <input type='button' onClick={handleAnswer} value={optionsArr[3]} className='options' />
           </div>
         </div>
       }
@@ -153,7 +208,7 @@ const Quiz = ({ source, id }) => {
         page === 5 &&
         <div className='subject'>
           {
-            source[page].type === 0 ? (<audio id="musicplayer" controls autoPlay onCanPlay = {handleTime}>
+            source[page].type === 0 ? (<audio id="musicplayer" controls autoPlay onCanPlay={handleTime}>
               <source src={path} />
             </audio>) : (<div className='title'><h1>{source[page].src}</h1></div>)
           }
@@ -169,7 +224,7 @@ const Quiz = ({ source, id }) => {
         page === 6 &&
         <div className='subject'>
           {
-            source[page].type === 0 ? (<audio id="musicplayer" controls autoPlay onCanPlay = {handleTime}>
+            source[page].type === 0 ? (<audio id="musicplayer" controls autoPlay onCanPlay={handleTime}>
               <source src={path} />
             </audio>) : (<div className='title'><h1>{source[page].src}</h1></div>)
           }
@@ -185,7 +240,7 @@ const Quiz = ({ source, id }) => {
         page === 7 &&
         <div className='subject'>
           {
-            source[page].type === 0 ? (<audio id="musicplayer" controls autoPlay onCanPlay = {handleTime}>
+            source[page].type === 0 ? (<audio id="musicplayer" controls autoPlay onCanPlay={handleTime}>
               <source src={path} />
             </audio>) : (<div className='title'><h1>{source[page].src}</h1></div>)
           }
@@ -201,7 +256,7 @@ const Quiz = ({ source, id }) => {
         page === 8 &&
         <div className='subject'>
           {
-            source[page].type === 0 ? (<audio id="musicplayer" controls autoPlay onCanPlay = {handleTime}>
+            source[page].type === 0 ? (<audio id="musicplayer" controls autoPlay onCanPlay={handleTime}>
               <source src={path} />
             </audio>) : (<div className='title'><h1>{source[page].src}</h1></div>)
           }
@@ -217,7 +272,7 @@ const Quiz = ({ source, id }) => {
         page === 9 &&
         <div className='subject'>
           {
-            source[page].type === 0 ? (<audio id="musicplayer" controls autoPlay onCanPlay = {handleTime}>
+            source[page].type === 0 ? (<audio id="musicplayer" controls autoPlay onCanPlay={handleTime}>
               <source src={path} />
             </audio>) : (<div className='title'><h1>{source[page].src}</h1></div>)
           }
@@ -230,7 +285,7 @@ const Quiz = ({ source, id }) => {
         </div>
       }
       {
-        page === 10 && <div id = 'finalPage'>{info.name} good job!!! your score is {info.score}</div>
+        page === 10 && <div id='finalPage'>{info.name} good job!!! your score is {info.score}</div>
       }
 
     </div>
